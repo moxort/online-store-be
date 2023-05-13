87259aa70512c2e2e30a7b6e12415a1e8b4611c5
Vladyslav Petlovyi <>
2023-05-13 15:40:09 +0300

    added firebase support to backend

--- src/routes/updateListing.js
+++ src/routes/updateListing.js
@@ -6,7 +6,7 @@ export const updateListingRoute = {
     handler: async (req, h) => {
         const { id } = req.params;
         const { name, description, price } = req.payload;
-        const userId = '1234';
+        const userId = '12345';
         await db.query(`
             UPDATE listings
                 SET name=?, description=?, price=?
--- src/server.js
+++ src/server.js
@@ -1,6 +1,12 @@
 import Hapi from "@hapi/hapi";
 import routes from "./routes/index.js";
 import {db} from "./database.js";
+import * as admin from "firebase-admin"
+import credentials from "../credentials.json";
+
+admin.initializeApp({
+    credential: admin.credential.cert(credentials);
+});
 
 let server;
 
